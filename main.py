9b5877ea1862034a0345586e90b289902b3c6296
scoder5 <scoding83@gmail.com>
2023-05-16 22:25:22 +0530

    New Commits

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template("index.html")
+
+@app.route('/analytics')
+def analytics():
+   return render_template("analytics.html")
+
+@app.route('/recom')
+def recom():
+   return render_template("recommendation.html")
+
+if __name__ == '__main__':
+   app.run(debug = True)
